8893d6297cd04b04c7b6332453038295300a78ea
caishaosmanper <122895259+caishaosmanper@users.noreply.github.com>
2024-02-29 23:03:41 -0500

    Update organized_layout.py

--- organized_layout.py
+++ organized_layout.py
@@ -9,8 +9,8 @@ def set_base():
     Display the foundational configuration and styling
     """
     # Set page configuration, background and header
-    st.set_page_config(page_title="ModelCraft Hub", page_icon='📊', layout='centered')
-    set_background("images/background_image.png")
+    st.set_page_config(page_title="Prediction of Wild Fires", page_icon='📊', layout='centered')
+    set_background("images/background_image (3).png")
     display_header("ModelCraft Hub",
                    "select, clean and visualize data for AI modeling", is_sidebar=False)
 
@@ -23,17 +23,23 @@ def set_base():
         </style>
         """, unsafe_allow_html=True)
     st.sidebar.image("images/logo.png", use_column_width=True)
-    st.sidebar.text("")
 
     # Hide hamburger icon menu & streamlit header
     hide_st_style = """
                 <style>
-                #MainMenu {visibility: hidden;}
                 footer {visibility: hidden;}
                 header {visibility: hidden;}
                 </style>
-                """
+                 """
     st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+@st.cache_data
+def load_data():
+    file_demo = random.randint(0, 1)
+    files = ['Data3/merged_fire_sample.csv', 'Data3/bikes_sharing.csv']
+    uploaded_data = pd.read_csv(files[file_demo])
+    return uploaded_data
 
 
 def upload_data():
@@ -42,8 +48,7 @@ def upload_data():
     -------
     :return pd.DataFrame or None -> The uploaded dataset or None if no dataset is uploaded.
     """
-    pd.set_option('display.max_rows', 10**10)
-    pd.set_option('display.width', 500)
+
     pd.set_option("styler.render.max_elements", 10**10)
 
     # Display header for Step 1
@@ -52,23 +57,33 @@ def upload_data():
     load = st.sidebar.checkbox('Use demo data', value=False, help="Click to use a dataset. The application will "
                                                                   "randomly choose between a fires dataset and a "
                                                                   "bike-sharing dataset")
-    file_demo = random.randint(0, 1)
-    files = ['Data3/merged_fire_sample.csv', 'Data3/bikes_sharing.csv']
     if load:
-        st.subheader("Step 1: Preview Data")
-        uploaded_data = pd.read_csv(files[file_demo])
-        st.write(uploaded_data)
+        # display header
+        st.divider()
+        st.subheader("1.  Preview Data")
+        st.divider()
+
+        # load and view data
+        uploaded_data = load_data()
+        st.dataframe(data=uploaded_data, width=700)
         st.session_state.data = uploaded_data
         return uploaded_data
     else:
         uploaded_data = st.sidebar.file_uploader("Choose a dataset", type="csv",)
 
         if uploaded_data:
-            st.subheader("Step 1: Preview Data")
+            # display header
+            st.divider()
+            st.subheader("1.  Preview Data")
+            st.divider()
+
+            # load and view data
             uploaded_data = pd.read_csv(uploaded_data)
             st.write(uploaded_data)
             st.session_state.data = uploaded_data
             return uploaded_data
+
         else:
             st.sidebar.warning("Please upload a dataset")
             return None
+
